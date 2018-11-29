a358d2cf9a79edf180ad700d8c01cd2ce50a59f3
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-11-29 09:03:05 +0100

    Aufgabe4

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    public static void main(String[] args)
+    {
+        String name = "hi";
+        int alter = 2;
+        printPerson( "Max" ,18);
+    }
+
+    public static void printPerson (String name, int alter)
+    {
+        System.out.println(name + " / "+ alter);
+
+
+    }
+
+}
