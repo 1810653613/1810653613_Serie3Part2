c82521ac40a0503ee36af176b2511c3153e8ce20
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-11-29 09:28:29 +0100

    Aufgabe5

---
+++ src/Rechnen.java
@@ -0,0 +1,19 @@
+public class Rechnen
+{
+    public static void main (String[] args)
+    {
+        System.out.println(add(1,2));
+        System.out.println(add(1.5,5.7));
+
+    }
+    public static int add(int a , int b)
+    {
+        return a+b;
+    }
+    public static double add(double a, double b)
+    {
+      return a+b;
+    }
+
+
+}
